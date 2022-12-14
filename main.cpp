048ffa633380f9eba5d282a1ce5287d7b1dde02b
mattianatale <117930083+mattianatale@users.noreply.github.com>
2022-12-14 12:09:43 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
 
 int main(){
+  char a[10][20];
+    char b[20];
+    for(int i=0; i<10; i++){
+        for(int j=0; j<20; j++){
+            a[i][j]=' ';
+            b[j]=' ';
+        }
+    }
+    for(int i=0; i<10; i++){
+        cin >> a[i];
+    }
+    cin >> b;
+
+    int p=0;
+    if(funzione(a, b, p)){
+        cout<<p;
+    }
+    else
+        cout<<"non presente";
   return 0;
 }
